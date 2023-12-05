9e491e03b219b2db5e7d54e3b2b19f740fe1d493
Md-Azad <sokalazad@gmail.com>
2023-12-06 00:10:05 +0100

    implemented delete operation from my booking page

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -2,47 +2,39 @@ import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
 import BookingRow from "./BookingRow";
 
-
 const Bookings = () => {
-    const {user} = useContext(AuthContext);
-    const [bookings, setBookings] = useState([]);
+  const { user } = useContext(AuthContext);
+  const [bookings, setBookings] = useState([]);
 
-    const url = `http://localhost:5000/booking?email=${user?.email}`
-    useEffect(()=>{
-        fetch(url)
-        .then(res=>res.json())
-        .then(data=>setBookings(data))
-    },[])
-    return (
-        <div className="overflow-x-auto">
-  <table className="table">
-    {/* head */}
-    <thead>
-      <tr>
-        <th>
-          <label>
-            <input type="checkbox" className="checkbox" />
-          </label>
-        </th>
-        <th>Name</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      {
-        bookings.map(booking=><BookingRow
-        key={booking._id}
-        booking={booking}
-        ></BookingRow>)
-      }
-      
-      
-    </tbody>
-  </table>
-</div>
-    );
+  const url = `http://localhost:5000/booking?email=${user?.email}`;
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setBookings(data));
+  }, []);
+  return (
+    <div className="overflow-x-auto">
+        <h1 className="text-2xl text-red-700">Bookings: {bookings.length}</h1>
+      <table className="table">
+    
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Service</th>
+            <th>Date</th>
+            <th>Price</th>
+            <th>Status</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {bookings.map((booking) => (
+            <BookingRow key={booking._id} booking={booking}></BookingRow>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Bookings;
